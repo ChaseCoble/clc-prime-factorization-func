875992569fead140a313738425b29364abbd8902
ChaseCoble <113311559+ChaseCoble@users.noreply.github.com>
2024-08-31 20:35:23 -0500

    Update factor_fn.py
    Optimization and implementing better user experience. Time Complexity of this algorithm (Sieve of Erasthotenes) is O(n log(log(n)))

--- factor_fn.py
+++ factor_fn.py
@@ -1,59 +1,48 @@
-from timeit import repeat
 import numpy
 
 
 def prime_factorize(target):
-  n = target + 1
+    n = target + 1
 
-  def primesfrom2to(n):
-    """ Input n>=6, Returns a array of primes, 2 <= p < n Code taken from https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n"""
-    sieve = numpy.ones(n // 3 + (n % 6 == 2), dtype=bool)
-    for i in range(1, int(n**0.5) // 3 + 1):
-      if sieve[i]:
-        k = 3 * i + 1 | 1
-        sieve[k * k // 3::2 * k] = False
-        sieve[k * (k - 2 * (i & 1) + 4) // 3::2 * k] = False
-    return numpy.r_[2, 3, ((3 * numpy.nonzero(sieve)[0][1:] + 1) | 1)]
+    def primesfrom2to(n):
+        sieve = numpy.ones(n // 3 + (n % 6 == 2), dtype=bool)
+        for i in range(1, int(n**0.5) // 3 + 1):
+            if sieve[i]:
+                k = 3 * i + 1 | 1
+                sieve[k * k // 3::2 * k] = False
+                sieve[k * (k - 2 * (i & 1) + 4) // 3::2 * k] = False
+        return numpy.r_[2, 3, ((3 * numpy.nonzero(sieve)[0][1:] + 1)   |1)]
 
-  prime_list = primesfrom2to(n).tolist()
-  counter = len(prime_list)
-  factor_list = [1]
-
-  while counter != 0:
+    prime_list = primesfrom2to(n).tolist()
+    counter = len(prime_list)
+    factor_list = [1]
     if target in prime_list:
-      factor_list.append(target)
-      counter = 0
-    elif target % prime_list[0] == 0:
-      factor_list.append(prime_list[0])
-      target = target // prime_list[0]
-      prime_list.pop(0)
-      for x in reversed(prime_list):
-        if x > target:
-          prime_list.remove(x)
-          counter = len(prime_list)
-          if counter == 0:
-            return factor_list
-          else:
-            break
+        factor_list.append(target)
+        counter = 0
+    while counter != 0:
+        if target in prime_list:
+            factor_list.append(target)
+            prime_list.remove(target)
+        elif target % prime_list[0] == 0:
+            factor_list.append(prime_list[0])
+            target = target // prime_list[0]
+            prime_list.pop(0)
         else:
-          break
-      counter = len(prime_list)
-    elif target % prime_list[0] != 0:
-      prime_list.pop(0)
-      counter = len(prime_list)
-      repeat
-    else:
-      return factor_list
-
-  return print(f'The prime factors are {factor_list}')
+            prime_list.pop(0)
+            for x in reversed(prime_list):
+                if x >= target or target % x != 0:
+                    prime_list.remove(x)
+        counter = len(prime_list)
+    return print(f'The prime factors are {factor_list}')
 
 def intCheck(inputVar):
-  isInt = False
-  while not isInt:
     if inputVar.isdigit():
-      return int(inputVar)
-    else:
-      inputVar = input("That was not a positive integer! Please enter the positive integer you intended: ")
+        if int(inputVar) <= 0:
+            inputVar = print("That was not a positive integer above zero!\n")
+            return False
+        return True
+    print("That was not even a number!")    
+    return False
                        
 
 def continuePrompt():
@@ -68,11 +57,15 @@ def continuePrompt():
 
 
 willContinue = True
+
 while willContinue:
-  print("This is a function that will find all prime factorizations of any target integer you wish to submit!")
-  targetInt = input("What number do you want to find the prime factors for?  ")
-  checkedInt = intCheck(targetInt)
-  prime_factorize(checkedInt)
-  willContinue = continuePrompt()
+    isInt = False
+    print("This is a function that will find all prime factorizations of any target integer above zero you wish to submit! \n")
+    while not isInt:
+        targetInt = input("What number do you want to find the prime factors for?  ")
+        isInt = intCheck(targetInt)
+    targetInt = int(targetInt)
+    prime_factorize(targetInt)
+    willContinue = continuePrompt()
   
 print("Thank you for trying my code!")
